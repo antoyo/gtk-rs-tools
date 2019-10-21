00eebe930fe168cab7a0cb405387cec21c22c3ec
Antoni Boucher <bouanto@zoho.com>
2019-10-21 11:34:31 -0400

    Implement for both Rc and Arc

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,57 @@
+use std::rc::{self, Rc};
+use std::sync::{self, Arc};
+
+pub trait Downgrade {
+    type Target;
+
+    fn downgrade(&self) -> Self::Target;
+}
+
+impl<T> Downgrade for Arc<T> {
+    type Target = sync::Weak<T>;
+
+    fn downgrade(&self) -> Self::Target {
+        Arc::downgrade(self)
+    }
+}
+
+impl<T> Downgrade for Rc<T> {
+    type Target = rc::Weak<T>;
+
+    fn downgrade(&self) -> Self::Target {
+        Rc::downgrade(self)
+    }
+}
+
+pub trait Upgrade {
+    type Target;
+
+    fn upgrade(&self) -> Option<Self::Target>;
+}
+
+impl<T> Upgrade for sync::Weak<T> {
+    type Target = Arc<T>;
+
+    fn upgrade(&self) -> Option<Self::Target> {
+        self.upgrade()
+    }
+}
+
+impl<T> Upgrade for rc::Weak<T> {
+    type Target = Rc<T>;
+
+    fn upgrade(&self) -> Option<Self::Target> {
+        self.upgrade()
+    }
+}
+
 #[macro_export]
 macro_rules! clone {
     ($($n:ident),+ => move |$($p:pat),*| $body:expr) => (
         {
-            $( let $n = ::std::rc::Rc::downgrade(&$n); )+
+            $( let $n = $crate::Downgrade::downgrade(&$n); )+
             move |$($p,)*| {
-                $(let $n = match $n.upgrade() {
+                $(let $n = match $crate::Upgrade::upgrade(&$n) {
                     Some(val) => val,
                     None => panic!("cannot upgrade weak reference `{}`", stringify!($n)),
                 };)+
